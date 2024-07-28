a65c129f12047154ab0a839941271f6a26b163d7
Joey Fawad <54484850+mojofawad@users.noreply.github.com>
2024-07-28 12:48:43 -0400

    simplify project structure (#1)

--- src/AmbrDM.Web/Program.cs
+++ src/AmbrDM.Web/Program.cs
@@ -1,38 +1,14 @@
-using AmbrDM.Web;
 using AmbrDM.Web.Components;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
-var oidcScheme = OpenIdConnectDefaults.AuthenticationScheme;
-
-// Add service defaults & Aspire components.
-builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddOutputCache();
-
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
-builder.Services.AddAuthentication(oidcScheme)
-    .AddKeycloakOpenIdConnect("keycloak", "AmbrDM", oidcScheme,
-        options =>
-        {
-            options.ClientId = "AmbrDM";
-            options.ResponseType = OpenIdConnectResponseType.Code;
-            options.RequireHttpsMetadata = false;
-        });
-
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -45,11 +21,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.UseOutputCache();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-app.MapDefaultEndpoints();
 
 app.Run();
